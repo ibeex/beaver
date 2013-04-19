795576c46e9f6db0a375b7510bf9268d4fa04b96
Ivan Brkanac <ibrkanac@gmail.com>
2013-04-19 15:37:47 +0200

    syslog

--- beaver/transports/syslog_transport.py
+++ beaver/transports/syslog_transport.py
@@ -1,37 +1,35 @@
 import socket
-import os
 
-import beaver.transport
+from beaver.transports.base_transport import BaseTransport
 
 
-class SyslogTransport(beaver.transport.Transport):
+class SyslogTransport(BaseTransport):
 
-    def __init__(self):
+    def __init__(self, beaver_config, logger=None):
+        super(SyslogTransport, self).__init__(beaver_config, logger=logger)
         FACILITY = {
             'kern': 0, 'user': 1, 'mail': 2, 'daemon': 3,
             'auth': 4, 'syslog': 5, 'lpr': 6, 'news': 7,
             'uucp': 8, 'cron': 9, 'authpriv': 10, 'ftp': 11,
             'local0': 16, 'local1': 17, 'local2': 18, 'local3': 19,
-            'local4': 20, 'local5': 21, 'local6': 22, 'local7': 23,
-        }
+            'local4': 20, 'local5': 21, 'local6': 22, 'local7': 23}
         LEVEL = {
             'emerg': 0, 'alert': 1, 'crit': 2, 'err': 3,
-            'warning': 4, 'notice': 5, 'info': 6, 'debug': 7
-        }
+            'warning': 4, 'notice': 5, 'info': 6, 'debug': 7}
 
-        self.syslog = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.host = os.environ.get("SYSLOG_ADDRESS", "127.0.0.1")
-        self.port = int(os.environ.get("SYSLOG_PORT", "514"))
-        self.level = LEVEL[os.environ.get("SYSLOG_LEVEL", "info")]
-        self.facility = FACILITY[os.environ.get("SYSLOG_FACILITY", "user")]
+        self._sock = socket.socket(socket.AF_INET,  # Internet
+                                   socket.SOCK_DGRAM)  # UDP
+        self._address = (beaver_config.get('syslog_host'), beaver_config.get('syslog_port'))
+        self.level = LEVEL[beaver_config.get('syslog_level')]
+        self.facility = FACILITY[beaver_config.get('syslog_facility')]
 
     def callback(self, filename, lines):
         for line in lines:
             msg = '<%d>%s:%s' % (self.level + self.facility * 8, filename, line)
-            self.syslog.sendto(msg, (self.host, self.port))
+            self._sock.sendto(msg, self._address)
 
     def interrupt(self):
-        self.syslog.close()
+        self._sock.close()
 
     def unhandled(self):
         return True
